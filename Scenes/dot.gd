d48c4df6b3537b7561d7c55633f997c6ce9c3ee6
Rip50 <rodionov0504@gmail.com>
2024-05-08 11:31:19 +0200

    Slow disappearance of the dot

--- Scenes/dot.gd
+++ Scenes/dot.gd
@@ -1,25 +1,28 @@
 extends Polygon2D
 
-@export var start_visible_time_sec: float
+@export var appearance_delay_sec: float
 @export var visibility_time_sec: float = 5
 
 var start_timer: Timer
 var disappear_timer: Timer
 
-func _init(start_visible_time_sec: float = 2) -> void:
-	self.start_visible_time_sec = start_visible_time_sec
+func _init(appearance_delay_sec: float = 2) -> void:
+	self.appearance_delay_sec = appearance_delay_sec
 	
 
 func _ready() -> void:
 	visible = false
+
 	start_timer = Timer.new()
 	start_timer.one_shot = true
 	add_child(start_timer)
+
 	disappear_timer = Timer.new()
 	disappear_timer.one_shot = true
 	add_child(disappear_timer)
+
 	start_timer.timeout.connect(_display)
-	start_timer.start(start_visible_time_sec)
+	start_timer.start(appearance_delay_sec)
 
 
 func _display() -> void:
@@ -27,6 +30,8 @@ func _display() -> void:
 	disappear_timer.timeout.connect(_disappear)
 	disappear_timer.start(visibility_time_sec)
 	
-	
+
 func _disappear() -> void:
-	queue_free()
+	var tween = get_tree().create_tween()
+	tween.tween_property(self, "self_modulate:a", 1, 0, 3)
+	tween.tween_callback(queue_free)
